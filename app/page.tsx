d7c37b825d03aada397e297e087df877d253d48a
Jari Suksi <jari.suksi@revonte.com>
2023-10-04 03:44:37 +0300

    Scroll cards to closest, when my location changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,24 @@ import { Backdrop, Map } from '@/components'
 import LocationCard from '@/components/LocationCard'
 import { useLocations } from '@/context/LocationContext';
 import { sortByDistance } from '@/utils'
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
-
-    const { locations } = useLocations()
+    const containerRef = useRef<HTMLInputElement>(null);
+    const { myLocation, locations } = useLocations()
     sortByDistance(locations);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (container) {
+            setTimeout(() => {
+                container.scrollTo({
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }, 240);
+        }
+    }, [myLocation, containerRef])
 
     return (
         <main className="overflow-hidden">
@@ -16,7 +29,7 @@ export default function Home() {
             <div className="bg-radial-gradient -mt-4">
                 <Map />
             </div>
-            <div className="absolute bottom-0 left-0 right-0 z-10 overflow-x-auto flex items-end snap-x p-4">
+            <div ref={containerRef} className="absolute bottom-0 left-0 right-0 z-10 overflow-x-auto flex items-end snap-x p-4">
                 {locations.map((location) => <LocationCard key={`location-${location.id}`} location={location} />)}
             </div>
         </main >
